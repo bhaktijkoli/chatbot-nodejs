085bfbcad977496c554ed11d7a8da37487c9044d
Bhaktij Koli <bhaktijkoli121@gmail.com>
2019-01-15 02:56:15 +0530

    Added home

--- client/js/app/Home/Home.jsx
+++ client/js/app/Home/Home.jsx
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
 
 class Home extends Component {
+  componentDidMount() {
+    document.getElementById('navbar').classList.remove('nav-raised');
+    document.getElementById('navbar').classList.add('nav-transparent');
+  }
   render() {
     return (
-      <div>
-        <h1>Welcome Home!</h1>
-      </div>
-    );
+      <main>
+        <div className="home-clip-path-1"></div>
+        <section id="welcome">
+          <div className="container">
+            <div className="row">
+              <div className="col-sm-6">
+                <h1>Turn visitors into leads and customers into happy, engaged users</h1>
+                <p>Modern products for sales, marketing and support to connect with customers and grow faster.</p>
+                <a className="btn white" href="#">Get Started</a>
+              </div>
+              <div className="col-sm-6">
+                <img src="/img/welcome.png" alt="Welcome" className="welcome-img" />
+                </div>
+              </div>
+            </div>
+          </section>
+          <div style={{height:2000}}></div>
+        </main>
+      );
+    }
   }
-}
 
-export default Home;
+  export default Home;
--- client/js/app/Layouts/Navbar.jsx
+++ client/js/app/Layouts/Navbar.jsx
@@ -1,23 +1,34 @@
 import React, { Component } from 'react';
 
 class Navbar extends Component {
+  componentDidMount() {
+    if(document.getElementsByClassName('nav-transparent').length === 1) {
+      window.addEventListener('scroll', function(e){
+        if(window.scrollY > 70) {
+          document.getElementById('navbar').className = "nav-raised";
+        } else {
+          document.getElementById('navbar').className = "nav-transparent";
+        }
+      });
+    }
+  }
   render() {
     return (
-      <nav id="navbar" class="nav-transparent nav-raised">
-        <div class="container">
-          <div class="nav-logo">
+      <nav id="navbar" className="nav-transparent">
+        <div className="container">
+          <div className="nav-logo">
             <a href="/">
               <img src="/img/logo_white.png" alt="Logo" />
             </a>
           </div>
-          <ul class="nav-items">
-            <li class="nav-item"><a href="">Home</a></li>
-            <li class="nav-item"><a href="/">Solutions</a></li>
-            <li class="nav-item"><a href="/">Pricing</a></li>
-            <li class="nav-item"><a href="">Login</a></li>
+          <ul className="nav-items">
+            <li className="nav-item"><a href="">Home</a></li>
+            <li className="nav-item"><a href="/">Solutions</a></li>
+            <li className="nav-item"><a href="/">Pricing</a></li>
+            <li className="nav-item"><a href="">Login</a></li>
           </ul>
         </div>
-        <div class="nav-item-space"></div>
+        <div className="nav-item-space"></div>
       </nav>
     );
   }
