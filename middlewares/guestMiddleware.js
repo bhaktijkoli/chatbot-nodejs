fc4cf6b40fe6b41478c8899472f152394fa18def
Bhaktij Koli <bhaktijkoli121@gmail.com>
2019-01-25 22:18:17 +0530

    Updated middleware

--- middlewares/guestMiddleware.js
+++ middlewares/guestMiddleware.js
@@ -1,13 +1,20 @@
 const tokenHandler = require('./../utils/token');
 
 module.exports = async (req, res, next) => {
+  let xmlRequest = false;
   if(req.headers['x-requested-with'] == 'XMLHttpRequest') {
     token = req.headers.authtoken;
+    xmlRequest = true;
   } else {
     token = req.cookies.authtoken;
-  }  tokenHandler.verifyAuthToken(token, (success, user) => {
+    xmlRequest = false;
+  }
+  tokenHandler.verifyAuthToken(token, (success, user) => {
     if(success) {
+      if(xmlRequest)
       res.status(401).send("Unauthorized")
+      else
+      res.redirect('/')
     } else {
       next();
     }
