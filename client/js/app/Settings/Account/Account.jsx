46fe0eb9007aa5426148b45a6c0e687b67d2a86e
Bhaktij Koli <bhaktijkoli121@gmail.com>
2019-02-06 15:42:37 +0530

    Added account password change api to frontend

--- client/js/app/Settings/Account/Account.jsx
+++ client/js/app/Settings/Account/Account.jsx
@@ -9,6 +9,7 @@ class Account extends Component {
     this.state = {
       nameChanged: false,
       emailChanged: false,
+      passwordChange: false,
     }
     this.checkChangeValues = this.checkChangeValues.bind(this);
   }
@@ -30,67 +31,72 @@ class Account extends Component {
             <p className="card-subtitle">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
             <form id="formUpdateAccount">
               <div className="input-group inline">
-                <div className="row">
-                  <div className="col-sm-2">
-                    <label htmlFor="firstname">Firstname</label>
-                  </div>
-                  <div className="col-sm-6">
-                    <input type="text" id="firstname" name="text" placeholder="Enter your firstname" onChange={this.checkChangeValues}/>
-                    <p className="help-block"></p>
-                  </div>
+                <div className="col-sm-2">
+                  <label htmlFor="firstname">Firstname</label>
+                </div>
+                <div className="col-sm-6">
+                  <input type="text" id="firstname" name="text" placeholder="Enter your firstname" onChange={this.checkChangeValues}/>
+                  <p className="help-block"></p>
                 </div>
               </div>
               <div className="input-group inline">
-                <div className="row">
-                  <div className="col-sm-2">
-                    <label htmlFor="lastname">Lastname</label>
-                  </div>
-                  <div className="col-sm-6">
-                    <input type="text" id="lastname" name="text" placeholder="Enter your lastname" onChange={this.checkChangeValues}/>
-                    <p className="help-block"></p>
-                  </div>
+                <div className="col-sm-2">
+                  <label htmlFor="lastname">Lastname</label>
+                </div>
+                <div className="col-sm-6">
+                  <input type="text" id="lastname" name="text" placeholder="Enter your lastname" onChange={this.checkChangeValues}/>
+                  <p className="help-block"></p>
                 </div>
               </div>
               <div className="input-group inline">
-                <div className="row">
-                  <div className="col-sm-6 offset-2">
-                    <If condition={this.state.nameChanged}>
-                      <button type="button" className="btn primary" onClick={this.onBasicUpdate.bind(this)}>Save</button>
-                    </If>
-                  </div>
+                <div className="col-sm-6 offset-2">
+                  <If condition={this.state.nameChanged}>
+                    <button type="button" className="btn primary" onClick={this.onBasicUpdate.bind(this)}>Save</button>
+                  </If>
                 </div>
               </div>
               <div className="divider"/>
               <div className="input-group inline">
-                <div className="row">
-                  <div className="col-sm-2">
-                    <label htmlFor="lastname">Email</label>
-                  </div>
-                  <div className="col-sm-6">
-                    <input type="email" id="email" name="email" onChange={this.checkChangeValues}/>
-                  </div>
+                <div className="col-sm-2">
+                  <label htmlFor="lastname">Email</label>
+                </div>
+                <div className="col-sm-6">
+                  <input type="email" id="email" name="email" onChange={this.checkChangeValues}/>
                 </div>
               </div>
               <div className="input-group inline">
-                <div className="row">
-                  <div className="col-sm-6 offset-2">
-                    <If condition={this.state.emailChanged}>
-                      <button type="button" className="btn primary" onClick={this.onEmailUpdate.bind(this)}>Update</button>
-                    </If>
-                  </div>
+                <div className="col-sm-6 offset-2">
+                  <If condition={this.state.emailChanged}>
+                    <button type="button" className="btn primary" onClick={this.onEmailUpdate.bind(this)}>Update</button>
+                  </If>
                 </div>
               </div>
               <div className="divider"/>
               <div className="input-group inline">
-                <div className="row">
-                  <div className="col-sm-2">
-                    <label htmlFor="lastname">Password</label>
-                  </div>
-                  <div className="col-sm-6">
-                    <a className="btn-text">Change password</a>
-                  </div>
+                <div className="col-sm-2">
+                  <label htmlFor="password">New Password</label>
+                </div>
+                <div className="col-sm-6">
+                  <input type="password" id="password" name="password" onChange={this.checkChangeValues}/>
                 </div>
               </div>
+              <If condition={this.state.passwordChange}>
+                <div className="input-group inline">
+                  <div className="col-sm-2">
+                    <label htmlFor="password">Confirm Password</label>
+                  </div>
+                  <div className="col-sm-6">
+                    <input type="password" id="password_confirm" name="password_confirm"/>
+                  </div>
+                </div>
+              </If>
+              <If condition={this.state.passwordChange}>
+                <div className="input-group inline">
+                  <div className="col-sm-6 offset-2">
+                    <button type="button" className="btn primary" onClick={this.onPasswordUpdate.bind(this)}>Update</button>
+                  </div>
+                </div>
+              </If>
             </form>
           </div>
         </div>
@@ -110,6 +116,11 @@ class Account extends Component {
       this.setState({emailChanged: true});
     } else {
       this.setState({emailChanged: false});
+    }
+    if(document.getElementById('password').value.length > 0) {
+      this.setState({passwordChange: true});
+    } else {
+      this.setState({passwordChange: false});
     }
   }
   onBasicUpdate() {
@@ -138,6 +149,29 @@ class Account extends Component {
       email:  document.getElementById('email').value,
     }
     axios.post(api('account/email/update'), data)
+    .then(res => {
+      if(fh.is_success(res.data)) {
+        Swal.SingleDialog.fire({
+          type: "info",
+          text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+        })
+      } else {
+        fh.set_multierrors(res.data);
+      }
+    })
+    .catch(err=> {fh.show_errorpage(err)})
+    .finally(() => {
+      fh.show_button();
+    })
+  }
+  onPasswordUpdate() {
+    fh.hide_button();
+    fh.remove_all_errros('formUpdateAccount');
+    let data = {
+      password:  document.getElementById('password').value,
+      password_confirm:  document.getElementById('password_confirm').value,
+    }
+    axios.post(api('account/password/update'), data)
     .then(res => {
       if(fh.is_success(res.data)) {
         Swal.SingleDialog.fire({
--- requests/accountPasswordUpdate.js
+++ requests/accountPasswordUpdate.js
@@ -3,13 +3,21 @@ const db = require('./../models');
 const rb = require('./../utils/response-builder');
 
 module.exports = (req, res, next) => {
-    req.check('password')
-        .notEmpty().withMessage('Password is required.');
+  req.check('password')
+  .notEmpty().withMessage('Password is required.');
+  req.check('password_confirm')
+  .notEmpty().withMessage('Password confirmation is required.')
+  .custom(val => {
+    return val == req.body.password;
+  }).withMessage("Password doesn't match")
 
-    if (rb.checkErrors(req, res)) return;
+  if (rb.checkErrors(req, res)) return;
 
-    req.data = {
-        password: req.user.password
-    };
-    next();
+  var salt = bcrypt.genSaltSync(10);
+  var password = bcrypt.hashSync(req.body.password, salt);
+
+  req.data = {
+    password: password
+  };
+  next();
 }
