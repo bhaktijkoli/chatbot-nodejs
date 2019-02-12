607cbe9f46c7cf007999049dd7543bd169502663
Bhaktij Koli <bhaktijkoli121@gmail.com>
2019-02-13 05:16:10 +0530

    Added Chat styles

--- client/js/chat.js
+++ client/js/chat.js
@@ -1,7 +1,15 @@
-
+var httpSessionUrl = 'http://api.chat.ai:3000/chat/session/get/';
 var chatContianer = document.querySelector('#chat-container');
 var chatWindow = document.querySelector('#chat-window');
 var chatWindowClose = document.querySelector('#chat-window-close');
+var chatHeader = document.querySelector('#chat-header');
+var chatWindowHeading = document.getElementById('chat-window-heading')
+var chatFooter = document.getElementById('chat-footer')
+var chatWindowInput = document.getElementById('chat-window-input')
+var chatWindowInputPlaceholder = document.querySelector('chat-window-input::placeholder')
+var chatMessageContainer = document.getElementById('chat-message-container');
+var chatbox = null;
+var session = null;
 
 var chat = {
   openWindow: function() {
@@ -18,15 +26,56 @@ var chat = {
   }
 };
 
-(function() {
+window.chat = {
+  init: function(key) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        var res = JSON.parse(this.responseText);
+        chatbox = res.chatbox;
+        session = res.session;
+        initChatbot();
+      }
+    };
+    xhttp.open("GET", httpSessionUrl+key, true);
+    xhttp.send();
+  }
+}
+
+function initChatbot() {
   chatContianer.addEventListener('click', function() {
     chat.openWindow();
   })
   chatWindowClose.addEventListener('click', function() {
     chat.closeWindow();
   })
-
+  chatWindowHeading.innerText = chatbox.title;
+  chatContianer.style.backgroundColor = chatbox.color;
+  chatHeader.style.backgroundColor = chatbox.color;
+  chatFooter.style.borderTopColor = chatbox.color;
+  insertMessage(chatbox.welcome_message, 'left');
   setTimeout(function () {
     chatContianer.classList.add("active");
   }, 500);
-})()
+  chatWindowInput.addEventListener('keyup', function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+      insertMessage(chatWindowInput.value, 'right');
+      chatWindowInput.value = "";
+     }
+  })
+}
+
+function insertMessage(text, pos) {
+  var messageItem = document.createElement('DIV');
+  messageItem.classList.add('message-item');
+  chatMessageContainer.appendChild(messageItem)
+  var message = document.createElement('DIV');
+  message.classList.add('message')
+  message.classList.add(pos)
+  if(pos=='left') {
+    message.style.backgroundColor = chatbox.color;
+  }
+  message.innerText = text;
+  messageItem.appendChild(message)
+}
